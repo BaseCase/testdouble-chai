1c85ff16bc60541a112add3ebc15afe1b03ab34d
Casey Brant <casey@caseybrant.com>
2015-11-09 18:42:18 -0600

    refactor: extract subject out to a beforeEach in the tests

--- test/testdouble-chai_test.js
+++ test/testdouble-chai_test.js
@@ -7,40 +7,45 @@ chai.use(tdChai);
 
 
 describe("testdouble-chai", function() {
+  beforeEach(function() {
+    this.subject = td.function("dubs");
+  });
+
   describe("using chai expect", function() {
     describe(".called", function() {
       it("is a property you can assert against in a chai expect chain", function() {
-        var test_double = td.function("dubs");
-        test_double();
-        expect(test_double).to.have.been.called;
+        this.subject();
+        expect(this.subject).to.have.been.called;
       });
 
       it("will fail with an AssertionError if the testdouble was not called", function() {
-        var test_double = td.function("dubs");
         expect(function() {
-          expect(test_double).to.have.been.called;
-        }).to.throw(chai.AssertionError, "AssertionError: expected " +
-                                         test_double +
-                                         " to have been called, but it was not.");
+          expect(this.subject).to.have.been.called;
+        }.bind(this))
+        .to.throw(chai.AssertionError,
+            "AssertionError: expected " +
+            this.subject +
+            " to have been called, but it was not.");
       });
 
       it("throws an exception if called on an object that isn't a testdouble");
       it("counts it as called regardless of arguments");
 
+
       describe("in a negated chain", function() {
         it("can assert that the testdouble wasn't called", function() {
-          var test_double = td.function("dubs");
-          expect(test_double).not.to.have.been.called;
+          expect(this.subject).not.to.have.been.called;
         });
 
         it("fails if you call the testdouble when you said you wouldn't", function() {
-          var test_double = td.function("dubs");
-          test_double();
+          this.subject();
           expect(function() {
-            expect(test_double).not.to.have.been.called;
-          }).to.throw(chai.AssertionError, "AssertionError: expected " +
-                                            test_double +
-                                            " not to have been called, but it was.");
+            expect(this.subject).not.to.have.been.called;
+          }.bind(this))
+          .to.throw(chai.AssertionError,
+              "AssertionError: expected " +
+              this.subject +
+              " not to have been called, but it was.");
         });
       });
     });
@@ -48,44 +53,45 @@ describe("testdouble-chai", function() {
 
     describe(".calledWith", function() {
       it("is a method in a chai expect chain that matches against function arguments", function() {
-        var test_double = td.function("dubs");
-        test_double("hi");
-        expect(test_double).to.have.been.calledWith("hi");
+        this.subject("hi");
+        expect(this.subject).to.have.been.calledWith("hi");
       });
 
       it("will fail on expected/actual arguments mismatch, detailing the discrepancies.", function() {
         // this is a pretty nasty assertion, but I wanted to make sure it captured the full error message
         // and don't want to pull in the actual chai string template functions to use here
-        var test_double = td.function("dubs");
-        test_double("what");
+        this.subject("what");
         expect(function() {
-          expect(test_double).to.have.been.calledWith("hi", "bye");
-        }).to.throw(chai.AssertionError, "AssertionError: expected " +
-                                         test_double +
-                                         " to have been called with " +
-                                         "[ \'hi\', \'bye\' ]," +
-                                         " but it received " +
-                                         "[ \'what\' ]");
+          expect(this.subject).to.have.been.calledWith("hi", "bye");
+        }.bind(this))
+        .to.throw(chai.AssertionError,
+            "AssertionError: expected " +
+            this.subject +
+            " to have been called with " +
+            "[ \'hi\', \'bye\' ]," +
+            " but it received " +
+            "[ \'what\' ]");
       });
 
       it("throws an exception if called on an object that isn't a testdouble");
 
+
       describe("in a negated chain", function() {
         it("can assert that the testdouble wasn't called with given args", function() {
-          var test_double = td.function("dubs");
-          test_double("bye");
-          expect(test_double).not.to.have.been.calledWith("hi");
+          this.subject("bye");
+          expect(this.subject).not.to.have.been.calledWith("hi");
         });
 
         it("fails if you call the testdouble with given args when you said you wouldn't", function() {
-          var test_double = td.function("dubs");
-          test_double("bye");
+          this.subject("bye");
           expect(function() {
-            expect(test_double).not.to.have.been.calledWith("bye");
-          }).to.throw(chai.AssertionError, "AssertionError: expected " +
-                                            test_double +
-                                            " not to have been called with [ \'bye\' ], " +
-                                            "but it was.");
+            expect(this.subject).not.to.have.been.calledWith("bye");
+          }.bind(this))
+          .to.throw(chai.AssertionError,
+              "AssertionError: expected " +
+              this.subject +
+              " not to have been called with [ \'bye\' ], " +
+              "but it was.");
         });
       });
     });
